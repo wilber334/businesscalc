da4a2596190fe5ea8ba9c8f8385aaf4ed775a409
wilber <wilberd334@gmail.com>
2023-09-16 10:52:55 -0500

    calculadora de negocios funcional beta

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Business Calc',
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
               seedColor: const Color.fromARGB(255, 0, 68, 255)),
